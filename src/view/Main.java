acce11625ca3824b2556460b84e0293c118e2c6d
Việt Lú <quocviet1509.krn@gmail.com>
2023-10-06 09:06:48 +0700

    update

---
+++ src/view/Main.java
@@ -0,0 +1,24 @@
+
+package view;
+
+import Controller.Manager;
+
+
+public class Main {
+      public static void main(String[] args) {
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.createNewAccount();
+                    break;
+                case 2:
+                    Manager.loginSystem();
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
+}
